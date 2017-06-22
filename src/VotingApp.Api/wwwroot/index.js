6a5cf9c098f16ffeeb6414cf16dba57ab406a915
paulopez78 <pau.lopez.78@gmail.com>
2017-06-22 12:50:52 +0200

    small UI refactoring

--- src/VotingApp.Api/wwwroot/index.js
+++ src/VotingApp.Api/wwwroot/index.js
@@ -6,38 +6,47 @@
         document.getElementById('errors').innerHTML = '';
         state.error 
             ? document.getElementById('errors').innerText = state.error
-            : renderUI(state);
-    }
+            : renderUI();
 
-    function renderUI(state) {
-        const isAdmin = window.location.search.indexOf('admin') !== -1;
-        if (!isAdmin) {
-            document.getElementById('votingCommands').style.display = 'none'; 
-        } 
+        function renderUI() {
+            renderAdmin();
+            renderVoting();
 
-        addEventHandler('start',
-            () => votingApi.start(document.getElementById('topics').value.split(','))
-                .then(render));
+            function renderAdmin()
+            {
+                const isAdmin = window.location.search.indexOf('admin') !== -1;
+                if (isAdmin) {
+                    document.getElementById('votingCommands').style.display = 'flex'; 
+                    addEventHandler('start',
+                        () => votingApi.start(document.getElementById('topics').value.split(','))
+                        .then(render));
+                    addEventHandler('finish',
+                        () => votingApi.finish()
+                        .then(render));
+                }
+            }
 
-        addEventHandler('finish',
-            () => votingApi.finish()
-                .then(render));
-
-        document.getElementById('votingTopics').innerHTML = '';
-        for (var topic in state.votes) {
-            document.getElementById('votingTopics').innerHTML +=
-                `<div id="${topic}" class="topic ${state.winner === topic ? 'selected' : ''}">
-                    ${topic}, votes: ${state.votes[topic]}
-                </div>`;
-        }
-        for (let topic in state.votes) {
-            addEventHandler(topic, () => votingApi.vote(topic).then(render), false);
+            function renderVoting()
+            {
+                document.getElementById('votingTopics').innerHTML = '';
+                for (var topic in state.votes) {
+                    document.getElementById('votingTopics').innerHTML +=
+                        `<div id="${topic}" class="topic ${state.winner === topic ? 'selected' : ''}">
+                            ${topic}, votes: ${state.votes[topic]}
+                        </div>`;
+                }
+                for (let topic in state.votes) {
+                    addEventHandler(topic, 
+                        () => votingApi.vote(topic)
+                            .then(render));
+                }
+            }
         }
     }
 
     const addEventHandler = (id, handler) => {
-        var el = document.getElementById(id),
-            elClone = el.cloneNode(true);
+        var el = document.getElementById(id);
+        var elClone = el.cloneNode(true);
         el.parentNode.replaceChild(elClone, el);
         document.getElementById(id).addEventListener('click', handler, false);
     }
