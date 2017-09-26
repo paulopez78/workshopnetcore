7a0f5b98fd2b00bdd2b8dce26b34a004c278fad7
paulopez78 <pau.lopez.78@gmail.com>
2017-09-26 11:52:39 +0200

    create different stages in docker file

--- src/VotingApp.Api/Program.cs
+++ src/VotingApp.Api/Program.cs
@@ -7,12 +7,7 @@ namespace VotingApp.Api
 {
    public class Program
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static void Main(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
